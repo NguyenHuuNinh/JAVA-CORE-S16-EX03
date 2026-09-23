1639c226c0beaf261cef800ca014cd276011f8fa
NguyenHuuNinh <nguyenhuuninh11032006@gmail.com>
2026-09-23 11:57:29 +0700

    Add AuditLogger class for event logging

---
+++ BE_Java_Core/Session16/Exercise03/AuditLogger.java
@@ -0,0 +1,12 @@
+package vn.edu.rikkei.session16.ex03;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class AuditLogger {
+    public static void logEvent(Supplier<String> timeSupplier, Consumer<String> logger, String eventName) {
+        String timestamp = timeSupplier.get();
+        String fullMessage = "[" + timestamp + "] - SỰ KIỆN: " + eventName;
+        logger.accept(fullMessage);
+    }
+}
